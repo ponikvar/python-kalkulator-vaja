ce392b5ad3b381252ee37c2cafd8f003ba649593
ponikvar <nikponikvar2007@gmail.com>
2026-03-16 20:31:48 +0100

    Dodana možnost izhoda iz programa (0) main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 def sestej(a, b):
     return a + b
-
 def odstej(a, b):
     return a - b
-
+def pomnozi(a, b):
+    return a * b
+def deli(a, b):
+    if b == 0:
+        print("Napaka: deljenje z 0 ni dovoljeno!")
+        return None
+    return a / b
 print("Pozdrav! Mini kalkulator")
+print("0 = izhod")
 print("1 = seštevanje")
 print("2 = odštevanje")
-
-izbira = input("Kaj želiš narediti? (1 ali 2): ")
-
-if izbira == "1":
+print("3 = množenje")
+print("4 = deljenje")
+izbira = input("Kaj želiš narediti? (0, 1, 2, 3 ali 4): ")
+if izbira == "0":
+    print("Izhod iz programa. Nasvidenje!")
+elif izbira == "1":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {sestej(x, y)}")
@@ -18,5 +26,15 @@ elif izbira == "2":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {odstej(x, y)}")
+elif izbira == "3":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {pomnozi(x, y)}")
+elif izbira == "4":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    rezultat = deli(x, y)
+    if rezultat is not None:
+        print(f"Rezultat: {rezultat}")
 else:
     print("Neveljavna izbira!")
